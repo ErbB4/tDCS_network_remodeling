8b43a905872dad179d14a16d0bfe95c29edc0306
Han Lu <miab736621299@gmail.com>
2023-04-15 16:20:25 +0200

    code to analyze single neuron firing rate

---
+++ Fig1/single_neuron/analysis-FR.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.optimize import leastsq
+
+import params
+reload (params)
+from params import *
+
+angles = np.arange(0.,2.*np.pi+1/96.*np.pi,1/96.*np.pi)
+for i in np.arange(30,48,1):
+    rates = []
+    for ang in angles:
+        extension = 'angle_'+str(ang)+'_current_'+str(i)+".npy"
+        times = np.load("times_"+extension)
+        
+        rate = len(times)/(simtime/1000.)
+        rates.append(rate)
+        os.remove("times_"+extension)
+        os.remove("senders_"+extension)
+
+    rates = np.array(rates)
+    np.save("rates_current_"+str(i)+".npy",rates)
+  
+
+
